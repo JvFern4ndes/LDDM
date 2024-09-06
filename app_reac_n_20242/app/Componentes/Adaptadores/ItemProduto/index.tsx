5ad774a88d8562074f839272095fd22a9a996675
João <jvfernandes.contato@gmail.com>
2024-09-06 20:29:36 -0300

    feat: implementation of exclude function

--- app_reac_n_20242/app/Componentes/Adaptadores/ItemProduto/index.tsx
+++ app_reac_n_20242/app/Componentes/Adaptadores/ItemProduto/index.tsx
@@ -2,7 +2,7 @@ import Produto from "@/app/Models/Produto";
 import Style from "@/app/Styles/Default";
 import React from "react";
 
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, Button } from "react-native";
 
 interface PropProd{
     produto:Produto,
@@ -13,12 +13,17 @@ const ItemProduto:React.FC<PropProd> = ( {produto})=> {
     
     console.log(produto)
     
+    function Excluir(id: number) {
+        throw new Error("Function not implemented.");
+    }
+
     return (
         <View style={Style.card} >
             <Text style={Style.cardText} >{produto.nome}</Text>
             <Text style={Style.cardText} >{produto.preco}</Text>
             <Image source={{uri:produto.foto} } 
              style={Style.image}   />
+            <Button title="Excluir" onPress={() => {Excluir(produto.id)}} />
             
         </View>
     )
