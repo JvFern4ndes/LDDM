d65c839ec7e3905411123116894cc5b52e6f478c
João <jvfernandes.contato@gmail.com>
2024-09-06 19:49:47 -0300

    feat: create a new state for preco

--- app_reac_n_20242/app/Componentes/Adaptadores/CadastroProduto/index.tsx
+++ app_reac_n_20242/app/Componentes/Adaptadores/CadastroProduto/index.tsx
@@ -6,6 +6,8 @@ import { Text, TextInput, View } from "react-native"
 const CadastroProduto = ()=>{
     let [nome,setNome] = useState('');
     let [descricao,setDescricao] = useState('');
+    let [preço, setPreco] = useState(0);
+
 //Continua Aqui
     return(
         <View>
@@ -13,6 +15,9 @@ const CadastroProduto = ()=>{
             <TextInput onChangeText={setNome} value={nome} />
             <Text   >Descricao:</Text>
             <TextInput onChangeText={setDescricao} value={descricao} />
+            <Text>Preço:</Text>
+            <TextInput onChangeText={(v) => setPreco(parseFloat(v))} inputMode="numeric" value={PerformanceObserver.toString()} />
+            <Button title="Cadastrar" onPress={Cadastrar}></Button>
             
         </View>
     )
