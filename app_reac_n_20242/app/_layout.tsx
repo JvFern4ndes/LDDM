2e0107810e9c92c5a5dbdbc41afc68beefd9064b
João <jvfernandes.contato@gmail.com>
2024-09-14 00:06:59 -0300

    feat: pagina de cadastrar produto

--- app_reac_n_20242/app/_layout.tsx
+++ app_reac_n_20242/app/_layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{title:"teste"}}  />
-      <Stack.Screen name="cadastro">{CadastroProduto}</Stack.Screen>
+      <Stack.Screen name="cadastro"   />
     </Stack>
   );
 }
--- app_reac_n_20242/app/cadastro.tsx
+++
@@ -1,7 +0,0 @@
-import CadastroProduto from "./Componentes/Adaptadores/CadastroProduto"
-
-export default function Cadastro() {
-    return (
-        <CadastroProduto />
-    )
-}
